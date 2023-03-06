3f309a7b59ce8b3d62deb10f864adcd2a07e4e5c
PRAKTIKAN <PRAKTIKAN@0605-PC-21>
2023-03-06 07:43:26 +0700

    commit1

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,20 @@
-namespace modul_3
+namespace MODUL_3
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- obj/Debug/net6.0-windows/modul_3.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/modul_3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("modul_3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MODUL_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("modul_3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("modul_3")]
+[assembly: System.Reflection.AssemblyProductAttribute("MODUL_3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MODUL_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
